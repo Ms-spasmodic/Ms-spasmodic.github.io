399113547946d84a94a324ac7fe078b164d6795d
Ms-spasmodic <ms.spasmodic@gmail.com>
2017-12-14 10:34:17 -0500

    Testing external sheet functions

--- javascript/Practice.js
+++ javascript/Practice.js
@@ -1,3 +1,3 @@
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Click me to display Date and Time.</button>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "You let me go :(";
+ }
