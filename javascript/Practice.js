42058bef4654e64fbbcd265de666789733937383
Ms-spasmodic <ms.spasmodic@gmail.com>
2017-12-29 15:36:59 -0500

    here we go

--- javascript/Practice.js
+++ javascript/Practice.js
@@ -3,7 +3,6 @@
 alert(document.lastModified);
 document.write(document.lastModified);
 function myFunction() {
-    alert(document.lastModified);
-    document.getElementById("demo").innerHTML = "I'll haunt you forever!";
-    
+    document.getElementById("demo").innerHTML = "You let me go :(";
+	alert("I'll haunt you forever!")
  }
