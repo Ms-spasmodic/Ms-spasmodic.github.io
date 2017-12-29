f4e6d4ac84b3511a912d21e2ad807d7935529559
Ms-spasmodic <ms.spasmodic@gmail.com>
2017-12-29 15:45:40 -0500

    i think its the broswer not the code, test in firefox

--- javascript/Practice.js
+++ javascript/Practice.js
@@ -1,5 +1,5 @@
 // testing my github
-
+document.write(document.lastModified);
 function myFunction() {
     document.getElementById("demo").innerHTML = "I'll haunt you forever!";
 	alert("you let me go :(")
