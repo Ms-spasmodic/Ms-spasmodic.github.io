dbf6c7c47776ac0261bf1aac794709890cd53783
Ms-spasmodic <ms.spasmodic@gmail.com>
2017-12-29 15:32:16 -0500

    alert didnt work

--- javascript/Practice.js
+++ javascript/Practice.js
@@ -4,5 +4,5 @@ alert(document.lastModified);
 document.write(document.lastModified);
 function myFunction() {
     document.getElementById("demo").innerHTML = "I'll haunt you forever!";
-    alert("You let me go :(");
+    alert(document.lastModified);
  }
