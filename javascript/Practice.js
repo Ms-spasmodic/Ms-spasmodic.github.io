6eda565b288c329ede8454e736487d247e09502e
Ms-spasmodic <ms.spasmodic@gmail.com>
2018-04-26 11:02:01 -0400

    fix error

--- javascript/Practice.js
+++ javascript/Practice.js
@@ -1,16 +1,17 @@
 // testing my github
 document.write(document.lastModified);
-function myFunction() {
-    document.getElementById("demo").innerHTML = "I'll haunt you forever!";
-	alert("you let me go :(")
- }
- function typeWriter() {
     var i = 0;
     var txt = 'Lorem ipsum dummy text blabla.';
     var speed = 50;
+function typeWriter() {
     if (i < txt.length) {
       document.getElementById("Type").innerHTML += txt.charAt(i);
       i++;
       setTimeout(typeWriter, speed);
     }
 }
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "I'll haunt you forever!";
+	alert("you let me go :(")
+ }
