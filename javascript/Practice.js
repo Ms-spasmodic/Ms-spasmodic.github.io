8e49bd2bf209b599a15c16f58c2916ac1a517298
Ms-spasmodic <ms.spasmodic@gmail.com>
2018-04-26 10:56:06 -0400

    move inline JS to External

--- javascript/Practice.js
+++ javascript/Practice.js
@@ -4,3 +4,13 @@ function myFunction() {
     document.getElementById("demo").innerHTML = "I'll haunt you forever!";
 	alert("you let me go :(")
  }
+ function typeWriter() {
+    var i = 0;
+    var txt = 'Lorem ipsum dummy text blabla.';
+    var speed = 50;
+    if (i < txt.length) {
+      document.getElementById("Type").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+}
